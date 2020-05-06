bdfaf18cdd9d595e17e72acb1cd2c8a4da4b5f1c
David Weber <dsw7@sfu.ca>
2020-05-06 09:10:29 +0100

    Uppercase consts

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 from met_aromatic import met_aromatic
 
 if __name__ == '__main__':
-    met_aromatic("1rcy".encode(), "A".encode(), 6.0)
+    TEST_CODE = "1rcy"
+    TEST_CHAIN = "A"
+    TEST_DISTANCE_CUTOFF = 6.0
+    met_aromatic(TEST_CODE.encode(), TEST_CHAIN.encode(), TEST_DISTANCE_CUTOFF)
