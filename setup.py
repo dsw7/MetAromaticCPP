d48da962155062c19a5893a0ca777418c7e9b1d0
David Weber <dsw7@sfu.ca>
2020-05-07 10:02:17 +0100

    Update src paths

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-CPP_COMPILE_V = 'std=c++17'
+CPP_COMPILE_V = '-std=c++17'
+PATH_HEADER_FILES = ['src']
 
 EXTERNAL_MODULES = [
     Extension(
@@ -10,7 +11,7 @@ EXTERNAL_MODULES = [
         ["met_aromatic_pyx.pyx", "met_aromatic.cpp"],
         language="c++",
         extra_link_args=["-lcurl"],  # add additional libs to link here
-        include_dirs=['src'],         # path to header files
+        include_dirs=PATH_HEADER_FILES,
         extra_compile_args=[CPP_COMPILE_V]
     )
 ]
