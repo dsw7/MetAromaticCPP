984f58f4e138c91f312c253548286595d083f737
David Weber <dsw7@sfu.ca>
2020-05-07 08:42:36 +0100

    Target C++17

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+CPP_COMPILE_V = 'std=c++17'
+
 EXTERNAL_MODULES = [
     Extension(
         "met_aromatic",
         ["met_aromatic_pyx.pyx", "met_aromatic.cpp"],
         language="c++",
         extra_link_args=["-lcurl"],  # add additional libs to link here
-        include_dirs=['src']         # path to header files
+        include_dirs=['src'],         # path to header files
+        extra_compile_args=[CPP_COMPILE_V]
     )
 ]
 
