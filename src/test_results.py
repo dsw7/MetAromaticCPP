08f6b44431d5dff11024540f33cf0509a9f7dfa3
David Weber <dsw7@sfu.ca>
2020-05-17 07:14:57 +0100

    Add results test

--- src/test_results.py
+++ src/test_results.py
@@ -2,7 +2,7 @@ from json import loads
 from pytest import mark
 from met_aromatic import met_aromatic
 
-TEST_DATA = [loads(line) for line in open('test_data.json')][25:75]
+TEST_DATA = [loads(line) for line in open('test_data.json')][25:60]
 TEST_CODES = [entry['_id'] for entry in TEST_DATA]
 TEST_CHAIN = 'A'
 TEST_DISTANCE_CUTOFF = 6.0
@@ -51,67 +51,3 @@ def test_met_aromatic(results_control):
         helper_test_met_phi(results_test, results_control)
         helper_test_met_residue_position(results_test, results_control)
         helper_test_aromatic_residue_position(results_test, results_control)
-
-def test_invalid_angle_exit_code():
-    assert met_aromatic(
-        '1rcy',
-        TEST_CHAIN,
-        TEST_DISTANCE_CUTOFF,
-        361.00
-    )['exit_code'] == 3
-
-def test_invalid_angle_exit_status():
-    assert met_aromatic(
-        '1rcy',
-        TEST_CHAIN,
-        TEST_DISTANCE_CUTOFF,
-        361.00
-    )['exit_status'] == "Invalid cutoff angle"
-
-def test_invalid_angle_exit_code_negative():
-    assert met_aromatic(
-        '1rcy',
-        TEST_CHAIN,
-        TEST_DISTANCE_CUTOFF,
-        -361.00
-    )['exit_code'] == 3
-
-def test_invalid_angle_exit_status_negative():
-    assert met_aromatic(
-        '1rcy',
-        TEST_CHAIN,
-        TEST_DISTANCE_CUTOFF,
-        -361.00
-    )['exit_status'] == "Invalid cutoff angle"
-
-def test_invalid_distance_exit_code():
-    assert met_aromatic(
-        '1rcy',
-        TEST_CHAIN,
-        -1.00,
-        TEST_ANGLE_CUTOFF
-    )['exit_code'] == 3
-
-def test_invalid_distance_exit_status():
-    assert met_aromatic(
-        '1rcy',
-        TEST_CHAIN,
-        -1.00,
-        TEST_ANGLE_CUTOFF
-    )['exit_status'] == "Invalid cutoff distance"
-
-def test_invalid_pdb_code_exit_code():
-    assert met_aromatic(
-        'abcd',
-        TEST_CHAIN,
-        TEST_DISTANCE_CUTOFF,
-        TEST_ANGLE_CUTOFF
-    )['exit_code'] == 4
-
-def test_invalid_pdb_code_exit_status():
-    assert met_aromatic(
-        'abcd',
-        TEST_CHAIN,
-        TEST_DISTANCE_CUTOFF,
-        TEST_ANGLE_CUTOFF
-    )['exit_status'] == "PDB entry does not exist"
